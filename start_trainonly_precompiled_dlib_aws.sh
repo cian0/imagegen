a49c81c3d1e70fb2851b1b93c7cdf0a76c3c83aa
cian0 <cyril.icasiano@gmail.com>
2022-12-29 18:10:44 +0800

    Update start_trainonly_precompiled_dlib_aws.sh

--- start_trainonly_precompiled_dlib_aws.sh
+++ start_trainonly_precompiled_dlib_aws.sh
@@ -224,29 +224,33 @@ touch ~/logs_training
 export OUTPUT_DIR_TRAINING="stable_diffusion_weights/output" 
 
 if [[ $GPU_NAME == *"NVIDIA A10G"* ]]; then
+    pip uninstall -y xformers
+
     accelerate launch train_dreambooth.py \
         --pretrained_model_name_or_path="$MODEL_NAME" \
         --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
         --output_dir=$OUTPUT_DIR_TRAINING \
         --revision="fp16" \
-        --with_prior_preservation --prior_loss_weight=1.0 \
         --seed=1337 \
         --resolution=512 \
         --train_batch_size=1 \
         --train_text_encoder \
         --center_crop \
         --mixed_precision="fp16" \
-        --gradient_accumulation_steps=2 \
+        --gradient_accumulation_steps=1 \
         --learning_rate=1e-6 \
         --lr_scheduler="constant" \
         --lr_warmup_steps=0 \
-        --num_class_images=80 \
         --sample_batch_size=4 \
         --max_train_steps=$STEPS_BASED_ON_FILES \
-        --save_interval=$STEPS_BASED_ON_FILES \
+        --save_interval=500 \
         --save_sample_prompt="photo of $MODEL_ID person" \
         --concepts_list="concepts_list.json" 
 
+        # --with_prior_preservation --prior_loss_weight=1.0 \
+        # --num_class_images=80 \
+        # --gradient_checkpointing \
+        # --use_8bit_adam \?
         # --gradient_checkpointing \
         # --not_cache_latents \
 fi > ~/logs_training
