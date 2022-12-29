b308e0912526e528389ec3ec8e98df96390feaea
cian0 <cyril.icasiano@gmail.com>
2022-12-29 22:25:06 +0800

    Update start_trainonly_precompiled_dlib_aws.sh

--- start_trainonly_precompiled_dlib_aws.sh
+++ start_trainonly_precompiled_dlib_aws.sh
@@ -69,7 +69,7 @@ fi
 
 aws configure set aws_access_key_id AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key AWS_SECRET_ACCESS_KEY
-aws configure set region "ap-southeast-1"
+aws configure set region "us-east-1"
 aws configure set output "json"
 
 cd ~
